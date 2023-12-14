caee783ac6361a4725499afca3ce615d0ed5916a
arne-p <78234073+pk-devs@users.noreply.github.com>
2023-12-14 13:59:35 +0100

    Fixed feedback

--- src/components/sidewidget/SidewidgetNews.jsx
+++ src/components/sidewidget/SidewidgetNews.jsx
@@ -1,13 +1,14 @@
-import { useSelector } from "react-redux"
+// import { useSelector } from "react-redux"
 import "./SidewidgetNews.css"
+import { NewsArticles } from "../../NewsArticles"
 
 export const SidewidgetNews = () => {
-  const { articles } = useSelector(state => state.api)
-    console.log(" Does it work?:", articles)
+  // const { articles } = useSelector(state => state.api)
+
   return (
     <div className="sidewidget-news">
         <h2>News about climate change</h2>
-        {articles.map(article => (
+        {NewsArticles.map(article => (
             <div className="sidewidget-news-article" key={article.title}>
                 <a href={article.url} target="_blank" rel="noreferrer">{article.title}</a>
             </div>
